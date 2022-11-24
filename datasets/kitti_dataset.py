4ccdf7aa508626d381d86be0de83fe6c3c513e98
smartadpole <smartadpole@gmail.com>
2022-11-24 18:11:22 +0800

    fix for kitti data read;change local k

--- datasets/kitti_dataset.py
+++ datasets/kitti_dataset.py
@@ -44,8 +44,7 @@ class KITTIDataset(MonoDataset):
             scene_name,
             "velodyne_points/data/{:010d}.bin".format(int(frame_index)))
 
-        # return os.path.isfile(velo_filename)
-        return False
+        return os.path.isfile(velo_filename)
 
     def get_color(self, folder, frame_index, side, do_flip):
         color = self.loader(self.get_image_path(folder, frame_index, side))
